0466f629a8f577f92e9db537162b3bfee91c66d7
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-01-22 18:00:28 -0500

    composer bootstrap

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
 
+define("_ROOTFOLDER",$_SERVER['DOCUMENT_ROOT']);
+
 $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/' :
+        http_response_code(200);
         require __DIR__ . '/views/index.php';
         break;
     case '' :
+        http_response_code(200);
         require __DIR__ . '/views/index.php';
         break;
     case '/about' :
+        http_response_code(200);
         require __DIR__ . '/views/about.php';
         break;
     default:
---
+++ templates/head.php
@@ -0,0 +1,8 @@
+<head>
+    <title>INFO1208-Week03-<?php echo _TITLE;?></title>
+    <meta charset="utf8"/>
+    <meta name="author" content="Carlos Ferraz"/>
+    <meta name="keywords" content="php info1208 fanshawe carlos ferraz php forms http requests"/>
+    <link rel="icon" href="../img/favicon.ico"/>
+    <link rel="stylesheet" href="../styles/main.css"/>
+</head>
